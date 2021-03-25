133cd2228153d0ee32738422e6dbb695f7c3affa
Dzheremilz <trempejeremie@gmail.com>
2021-03-25 17:57:11 +0100

    updated option *

--- wc.js
+++ wc.js
@@ -1,28 +1,50 @@
 const fs = require('fs')
 
-if (process.argv.length !== 3) {
-  console.log('usage: node wc.js file.txt')
+let noOption = true
+
+if (process.argv.length !== 3 && !(process.argv.length === 4 && process.argv[2][0] === '-')) {
+  console.log('usage: node wc.js [-option (l, w, c)] file.txt')
+  process.exit(1)
+}
+let x = 0
+if (process.argv[2][0] === '-' && !(process.argv[2][1] === 'l' || process.argv[2][1] === 'w' || process.argv[2][1] === 'c')) {
+  console.log(`Error: ${process.argv[2]} is not an option`)
   process.exit(1)
 }
 
-if (!fs.existsSync(process.argv[2])) {
-  console.log(`Error: ${process.argv[2]} does not exist`)
+if (process.argv[2][0] === '-') {
+  x = 1
+  noOption = false
+}
+
+if (!fs.existsSync(process.argv[2 + x])) {
+  console.log(`Error: ${process.argv[2 + x]} does not exist`)
   process.exit(1)
 }
 
-if (!fs.statSync(process.argv[2]).isFile()) {
-  console.log(`Error: ${process.argv[2]} is not a file`)
+if (!fs.statSync(process.argv[2 + x]).isFile()) {
+  console.log(`Error: ${process.argv[2 + x]} is not a file`)
   process.exit(1)
 }
 
 let line = 0
 let word = 0
 let char = 0
+let str = ' '
 
-let text = fs.readFileSync(process.argv[2], 'utf-8')
+let text = fs.readFileSync(process.argv[2 + x], 'utf-8')
 
-line = text.split('\n').length
-word = text.split(' ').length
-char = text.length
+if (process.argv[2][1] === 'l' || noOption) {
+  line = text.split('\n').length
+  str += line + ' '
+}
+if (process.argv[2][1] === 'w' || noOption) {
+  word = text.split(' ').length
+  str += word + ' '
+}
+if (process.argv[2][1] === 'c' || noOption) {
+  char = text.length
+  str += char + ' '
+}
 
-console.log(` ${line} ${word} ${char} ${process.argv[2]}`)
+console.log(`${str}${process.argv[2 + x]}`)
